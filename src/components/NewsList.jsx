91bd56c3322de1e3e55433aad6701981d0a35edf
WillyMayer <will.mayer@outlook.com>
2023-12-13 22:50:10 -0300

    checkpoint

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -6,19 +6,31 @@ const NewsList = (props) => {
   console.log(articles);
   return (
     <div className="news-list my-5">
-      <div className="news-msg">
-        <h1 className="text-6xl font-bold text-center">Last News!</h1>
+      <div className="news-msg py-3">
+        <h1 className="text-6xl font-bold text-center font-playfair-display uppercase underline">Last News!</h1>
       </div>
       <div className="news-articles flex flex-wrap place-content-center ">
-        {articles.map((article) => (
-          <li className="article-item list-none p-4 w-2/5 h-50 mx-10 my-10 shadow-2xl ">
-            <img src={article.urlToImage} alt={article.description} className="border-2 border-gray-300"/>
-            <h2 className="text-3xl font-bold font my-3">{article.title}</h2>
-            <h3 className="italic">{article.source.name}</h3>
-            <p>{article.description}</p>
-            <a href={article.url} className="text-blue-400 font-bold">More Info</a>
-          </li>
-        ))}
+        <ul className="flex flex-wrap justify-center">
+          {articles.map((article) => (
+            <li className="article-item list-none md:w-4/5 lg:w-6/12">
+              <div className="div-article-item border-2 border-gray-200 p-4 h-auto mx-8 my-8 shadow-2xl">
+                <img
+                  src={article.urlToImage}
+                  alt={article.description}
+                  className="border-2 border-gray-300"
+                />
+                <h2 className="text-3xl font-bold font my-3">
+                  {article.title}
+                </h2>
+                <h3 className="italic">{article.source.name}</h3>
+                <p>{article.description}</p>
+                <a href={article.url} className="text-blue-400 font-bold">
+                  More Info
+                </a>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'playfair-display': ['Playfair Display', 'cursive'],
+      }
+    },
   },
   plugins: [],
 }
